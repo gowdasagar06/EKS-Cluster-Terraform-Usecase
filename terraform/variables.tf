35f17ca2d70fd931068569738e36839e96d6c570
gowdasagar06 <sagargowda6666@gmail.com>
2024-02-07 00:22:25 +0530

    push

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,3 +6,10 @@ variable "vpc_cidr" {
   type    = string
   default = "10.0.0.0/16"
 }
+
+# variable "cluster_name" {
+#   default = "demo"
+#   type = string
+#   description = "AWS EKS CLuster Name"
+#   nullable = false
+# }
